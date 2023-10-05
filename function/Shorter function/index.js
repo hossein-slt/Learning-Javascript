fbaae4d15e62dfbb5e537fae7e1a6205376a7b94
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-05 15:41:08 +0330

    Functions

---
+++ function/Shorter function/index.js
@@ -0,0 +1,6 @@
+
+console.log(isLandscape(850, 600))
+
+function isLandscape(width, height) {
+    return(width > height)
+}
---
+++ function/index.js
@@ -0,0 +1,8 @@
+let number = max(77, 17);
+console.log(number);
+
+function max(a, b) {
+    if (a > b) return a;
+    else if (a < b) return b;
+    else if (a = b) return "Equal";
+}
