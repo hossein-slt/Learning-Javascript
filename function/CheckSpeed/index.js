9a6264adbd394cfad399d9c7b4c77314e16e19a6
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-06 15:08:57 +0330

    Added Speedometer

---
+++ function/CheckSpeed/index.js
@@ -0,0 +1,25 @@
+
+
+checkSpeed(105)
+
+function checkSpeed(speed) {
+
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+        console.log("Ok");
+        return;
+    }
+
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+    if (points >= 12)
+        console.log("Suspended");
+
+    else
+        console.log("points", points);    
+}
+
+
+
