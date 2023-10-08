e8a0fbb28c29ca72dd0feb46302872abc6af5c48
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-08 16:54:19 +0330

    Added new exercise

---
+++ objects/Show address/index.js
@@ -0,0 +1,20 @@
+
+const address = {
+    street : "a",
+    city : "b",
+    zipcode : "c"
+}
+
+
+function showAddress(address) {
+    for(let key in address)
+        console.log(key, address[key]);
+}
+
+
+showAddress(address);
+
+
+
+
+
