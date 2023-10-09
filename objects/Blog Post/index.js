d7b70795d9ee78cb40992841df525f47e9a0feef
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-09 08:45:42 +0330

    Create Blog Post

---
+++ objects/Blog Post/index.js
@@ -0,0 +1,16 @@
+
+let post = {
+    title : "a",
+    body : "b",
+    author : "c",
+    views : 15,
+    comments : [
+        {author : "a", body : "b"},
+        {author : "c", body : "d"}
+    ],
+    isLive : true
+}
+
+console.log(post);
+
+
