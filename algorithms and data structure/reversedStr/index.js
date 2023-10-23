361fcd095ee92171398b4908cf972279a1dd22e1
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-23 10:29:25 +0330

    Added new exercise

---
+++ algorithms and data structure/reversedStr/index.js
@@ -0,0 +1,10 @@
+// reversed a string.
+
+function reversedStr(str) {
+    let reversed = "";
+    for(let i = str.length -1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+console.log(reversedStr("hello"));
