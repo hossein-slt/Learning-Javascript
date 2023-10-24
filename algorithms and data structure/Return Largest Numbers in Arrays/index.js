218f142f94faa88772e7aecea5760cf454308feb
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-24 10:32:04 +0330

    Two solutions to return largest number

--- algorithms and data structure/Return Largest Numbers in Arrays/index.js
+++ algorithms and data structure/Return Largest Numbers in Arrays/index.js
@@ -1,3 +1,5 @@
+// There are two Declarative approaches to solve this issue.
+// First is to use map and reduce.
 
 function largestnumber(arr) {
     return arr.map(function(group) {
@@ -9,4 +11,10 @@ function largestnumber(arr) {
 
 console.log(largestnumber([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
+// Here it is possible to use apply and bind method.
 
+function largestnum(arr) {
+    return arr.map(Function.apply.bind(Math.max, null));
+}
+
+console.log(largestnum([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
