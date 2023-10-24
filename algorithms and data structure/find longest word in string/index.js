f8f9d60541ee7e103d40bce009e1d612bd47abfb
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-24 09:18:45 +0330

    Shorter and cleaner solution

--- algorithms and data structure/find longest word in string/index.js
+++ algorithms and data structure/find longest word in string/index.js
@@ -1,4 +1,4 @@
-
+// First solution is to use 'For' loop.
 function findLongestword(str) {
     let words = str.split(' ');
     let maxlength = 0;
@@ -12,3 +12,13 @@ function findLongestword(str) {
 }
 
 console.log(findLongestword("Hi my name is Hossein"))
+
+// It is also possible to use reduce() method. which shorter and cleaner.
+
+function longestword(str) {
+    return str
+        .split(' ')
+        .reduce((longest, word) => Math.max(longest, word.length), 0);
+}
+
+console.log(longestword("Hi my name is Hossein"))
