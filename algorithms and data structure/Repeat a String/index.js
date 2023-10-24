c2602f39e8aa9e435c5433b22bfd5e91b56ab035
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-24 14:50:07 +0330

    Added recursive solution

--- algorithms and data structure/Repeat a String/index.js
+++ algorithms and data structure/Repeat a String/index.js
@@ -1,3 +1,5 @@
+// There are two solutions:
+// The first one is to use 'for' loop.
 
 function repeatString(str, num) {
     let accumulator = "";
@@ -9,3 +11,11 @@ function repeatString(str, num) {
 }
 
 console.log(repeatString("abc", 3))
+
+// It is also possible to use Recursive Solution.
+
+function repeatstr(str, num) {
+    return num > 0 ? str + repeatstr(str, num - 1) : '';
+}
+
+console.log(repeatstr("abc", 3))
