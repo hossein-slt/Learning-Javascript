14da5580ffc23901cc48bd0d1b824abd12c60b74
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-24 10:14:54 +0330

    Return largest number in arrays

---
+++ algorithms and data structure/Return Largest Numbers in Arrays/index.js
@@ -0,0 +1,12 @@
+
+function largestnumber(arr) {
+    return arr.map(function(group) {
+        return group.reduce(function(prev, current) {
+            return current > prev ? current : prev;
+        });
+    });
+}
+
+console.log(largestnumber([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
