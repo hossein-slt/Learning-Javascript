5784868a6c36c0739f7800328286284532b99cb5
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-25 09:28:41 +0330

    Use chain method for title case

--- algorithms and data structure/Title Case a Sentence/index.js
+++ algorithms and data structure/Title Case a Sentence/index.js
@@ -1,4 +1,5 @@
-
+// There are 2 solutions
+// First one is to use 'for' loop.
 function titleCase(str) {
     const newtitle = str.split(" ");
     const updatedtitle = [];
@@ -10,3 +11,14 @@ function titleCase(str) {
 
 console.log(titleCase("hello everybody my name is hossein"))
 
+// Here we can use chain method.
+
+function titlecaseupper(str) {
+    return str
+        .toLowerCase()
+        .split(" ")
+        .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+        .join(" ")
+}
+
+console.log(titlecaseupper("hello everyone my name is hossein"))
