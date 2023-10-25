2e21fc6258e5fb498ff9c6cd18281df682972a06
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-25 16:25:47 +0330

    Spread method

--- algorithms and data structure/Slice and Splice/index.js
+++ algorithms and data structure/Slice and Splice/index.js
@@ -1,3 +1,5 @@
+// Three solutions is possible
+// First is to use 'for' loop.
 
 function frankenSplice(arr1, arr2, n) {
     let localarray = arr2.slice();
@@ -11,3 +13,12 @@ function frankenSplice(arr1, arr2, n) {
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 
+// Here we can use spread method. which is shorter and cleaner.
+
+function franksplice(arr1, arr2, n) {
+    let localarray = arr2.slice();
+    localarray.splice(n, 0, ...arr1);
+    return localarray;
+}
+
+console.log(franksplice([1, 2, 3], [4, 5, 6], 1));
