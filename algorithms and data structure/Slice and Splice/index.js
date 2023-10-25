9bc727b186aad5be03bcde81f2fedc7d46a89af7
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-25 16:33:59 +0330

    Slice and Splice

--- algorithms and data structure/Slice and Splice/index.js
+++ algorithms and data structure/Slice and Splice/index.js
@@ -22,3 +22,11 @@ function franksplice(arr1, arr2, n) {
 }
 
 console.log(franksplice([1, 2, 3], [4, 5, 6], 1));
+
+// It is also possible not to use splice() method for shorter and cleaner.
+
+function frankenslice(arr1, arr2, n) {
+    return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+}
+
+console.log(frankenslice([1, 2, 3], [4, 5, 6], 1));
