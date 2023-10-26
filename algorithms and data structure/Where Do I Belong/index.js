4c3d472675c10f0404b9f66deb1c50e354666e9e
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-26 10:09:19 +0330

    Try to use filter method

--- algorithms and data structure/Where Do I Belong/index.js
+++ algorithms and data structure/Where Do I Belong/index.js
@@ -1,3 +1,5 @@
+// There are three solutions to solve this issue.
+// First is to use 'for' loop.
 
 function getIndextoins(arr, num) {
     arr.sort((a, b) => a - b);
@@ -10,3 +12,10 @@ function getIndextoins(arr, num) {
 
 console.log(getIndextoins([30, 50, 20, 10, 70, 60,], 40));
 
+// Here we can use filter() method.
+
+function getindextoin(arr, num) {
+    return arr.filter(val => num > val).length;
+}
+
+console.log(getindextoin([30, 50, 20, 10, 70, 60,], 40));
