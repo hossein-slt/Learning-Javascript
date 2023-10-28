9a9039b964a98a31798bc2c66fa5de7d9321efd5
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-10-28 11:35:19 +0330

    Use chain method to mutation

--- algorithms and data structure/Mutation/index.js
+++ algorithms and data structure/Mutation/index.js
@@ -1,3 +1,4 @@
+// The first solution is to use 'for' loop.
 
 function mutation(arr) {
     const test = arr[1].toLowerCase();
@@ -10,6 +11,15 @@ function mutation(arr) {
 
 console.log(mutation(["HELLO", "HEY"]));
 
+// Here we can also use chain method.
 
+function mute(arr) {
+    return arr[1]
+        .toLowerCase()
+        .split("")
+        .every(function(letter){
+           return arr[0].toLowerCase().indexOf(letter) 
+        });
+}
 
-
+console.log(mute(["GOODBYE", "BYE"]));
