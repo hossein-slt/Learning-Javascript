56fddd789840dab70a0fe65cef79e2d2747dbe8d
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-11-01 09:58:26 +0330

    Use some and every method

--- algorithms and data structure/Use every()  method/index.js
+++ algorithms and data structure/Use every()  method/index.js
@@ -15,3 +15,16 @@ function checknotnegative(arr) {
 }
 
 console.log(checknotnegative([1, 2, 3, 4, 5]));
+
+//////\\\\\\
+
+/*
+The some method works with arrays to check if any element passes a particular test. 
+It returns a Boolean value - true if any of the values meet the criteria, false if not.
+*/
+
+function checkodd(arr) {
+    return arr.some(val => val % 2 !== 0);
+}
+
+console.log(checkodd([2, 6, 4, 8, 12]));
