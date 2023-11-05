fcb363e4a4c144cdbadc6cd757ded804ce61d444
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-11-05 16:53:36 +0330

    Diff two arrays

--- Intermediate Algorithm Scripting/Diff two arrays/index.js
+++ Intermediate Algorithm Scripting/Diff two arrays/index.js
@@ -1,3 +1,5 @@
+// Two solutions are possible
+// first to use 'for' loop.
 
 function diffarray(arr1, arr2) {
     const newarr = [];
@@ -15,3 +17,14 @@ function diffarray(arr1, arr2) {
 }
 
 console.log(diffarray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// Here it is possible to use chain method.
+
+function diffArray(arr1, arr2) {
+    return arr1
+        .concat(arr2)
+        .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
