9b06d6c89c07486dc298918285d98db8620882f6
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-11-07 19:22:48 +0330

    Seek and destroy

--- Intermediate Algorithm Scripting/Seek and Destroy/index.js
+++ Intermediate Algorithm Scripting/Seek and Destroy/index.js
@@ -1,4 +1,4 @@
-
+// Two solutions are possible.
 function destroyer(arr) {
     const valstoremove = Array.from(arguments).slice(1);
     return arr.filter(function(val) {
@@ -6,5 +6,11 @@ function destroyer(arr) {
     })
 }
 
-
 console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+
+// The second one is much more cleaner and easier.
+function destroyer1(arr, ...valstoremove) {
+    return arr.filter(elem => !valstoremove.includes(elem));
+};
+
+console.log(destroyer1([1, 2, 3, 5, 1, 2, 3], 2, 3));
