ee5a341367f320f6737d9be1a7fd4d415d116460
Hossein Soltani <hossein.slt.1992@gmail.com>
2023-11-25 08:04:14 +0330

    Palindrome checker

---
+++ Intermediate Algorithm Scripting/Palindrome checker/index.js
@@ -0,0 +1,16 @@
+// It shows if a string is the same as it is written vice versa.
+
+function palindrome(str) {
+    let re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    let len = str.length;
+    for (let i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(palindrome('eye'));
+console.log(palindrome('head'));
