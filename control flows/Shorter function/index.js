035aa754a3047f9611b2d19be0fbae6b0ae06b22
Hossein Soltani <106621644+hossein-slt@users.noreply.github.com>
2025-07-06 10:01:17 +0330

    Update index.js
    shorter and cleaner way

--- control flows/Shorter function/index.js
+++ control flows/Shorter function/index.js
@@ -4,3 +4,10 @@ console.log(isLandscape(850, 600))
 function isLandscape(width, height) {
     return(width > height)
 }
+
+
+// it is also possible to use a shorter and cleaner way
+
+function isLandScape(width, height) {
+    return (width > height) ? true : false;
+}    
